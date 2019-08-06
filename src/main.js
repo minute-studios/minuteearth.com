58858df84ffd3fbb628f2af80b36d5a686c4d40c
Jasper <well.caffeinated@gmail.com>
2019-08-06 17:24:13 -0600

    added profile component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Buefy from 'buefy'
 import App from './App.vue'
 import ResponsiveEmbed from '@/components/responsive-embed'
 import LatestVideos from '@/components/latest-videos'
+import MEProfile from '@/components/me-profile'
 import VueYoutube from 'vue-youtube'
 
 import './styles/main.scss'
@@ -18,6 +19,7 @@ Vue.use(VueYoutube)
 Vue.use(Filters)
 Vue.component('responsive-embed', ResponsiveEmbed)
 Vue.component('latest-videos', LatestVideos)
+Vue.component('me-profile', MEProfile)
 
 Vue.config.productionTip = false
 
