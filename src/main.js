ad25c0652d88bb5bf0fc08d730aeee600dc5b80d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-06 00:39:53 +0000

    Deploy minute-studios/minuteearth.com to github.com/minute-studios/minuteearth.com.git:master

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import ResponsiveEmbed from '@/components/responsive-embed'
 import LatestVideos from '@/components/latest-videos'
 import MEProfile from '@/components/me-profile'
+import BookCarousel from '@/components/book-carousel'
+import LessonPlanCard from '@/components/lesson-plan-card'
 import VueYoutube from 'vue-youtube'
 
 import './styles/main.scss'
@@ -20,6 +22,8 @@ Vue.use(Filters)
 Vue.component('responsive-embed', ResponsiveEmbed)
 Vue.component('latest-videos', LatestVideos)
 Vue.component('me-profile', MEProfile)
+Vue.component('book-carousel', BookCarousel)
+Vue.component('lesson-plan-card', LessonPlanCard)
 
 Vue.config.productionTip = false
 
